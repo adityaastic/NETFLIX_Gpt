03b5f2a4ce83cd512a2c92c39a6a9adc3a3f6e22
adityaastic <adityaastic@gmail.com>
2024-04-22 00:07:27 +0530

    store user info in store / navigate to browse

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import { Provider } from 'react-redux';
 import Body from './components/Body';
-import Header from './components/Header';
+// import Header from './components/Header';
+import appStore from './utils/appStore';
 
 function App() {
   return (
-    <div className=''>
-      <Header />
+    <Provider store={appStore}>
+      {/* <Header /> */}
       <Body />
-    </div>
+    </Provider>
   );
 }
+
 export default App;
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,28 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Login from "./Login";
 import Browse from "./Browse";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useDispatch } from "react-redux";
+import { adduser, removeUser } from "../utils/userSlice";
 
 const Body = () => {
-  const appRouter = createBrowserRouter([
-    {
-      path: "/",
+    const dispatch = useDispatch();
 
-      element: <Login />,
-    },
+    const appRouter = createBrowserRouter([
+        {
+            path: "/",
+            element: <Login />
+        },
+        {
+            path: "browse",
+            element: <Browse />
+        }
+    ])
 
-    {
-      path: "browse",
+    // using fire base API to store all the values into store
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                const { uid, email, displayName } = user;
+                dispatch(adduser({ uid: uid, email: email, displayName: displayName }))
+            } else {
+                // User is signed out
+                dispatch(removeUser());
+            }
+        });
+    }, [])
+    return (
+        <div>
+            <RouterProvider router={appRouter} />
 
-      element: <Browse />,
-    },
-  ]);
-
-  return (
-    <div>
-      <RouterProvider router={appRouter} />
-    </div>
-  );
+        </div>
+    );
 };
-
 export default Body;
