b94156f1c27cf88b2f1cb9617c5b4b08573fd8f0
adityaastic <adityaastic@gmail.com>
2024-04-22 01:48:07 +0530

    created the movieSlice and store

---
+++ src/utils/constants.js
@@ -0,0 +1,24 @@
+export const
+
+    LOGO_URL = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+
+export const
+
+    USER_ICON = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZ3SFVmXoYNHl2D22fjIEAwMuEqrbDYiUUwsWi6-K0AEnh9QZzAhgaOayZ6hFG4Eh_1m4&usqp=CAU";
+
+
+
+
+export const API_OPTIONS = {
+
+    method: 'GET',
+
+    headers: {
+
+        accept: 'application/json',
+
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ODU5OTk0MDkzZjU2MTk4MWI3NDJhYzRmOTIxYzQ2MyIsInN1YiI6IjY1YmY2NGQ0NDM5OTliMDE4NGM3ZDgyNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8jbaAzQngE83ebKFTVDNQ6a6CtXNt-H9fiFiYVEjPlI'
+
+    }
+
+};
