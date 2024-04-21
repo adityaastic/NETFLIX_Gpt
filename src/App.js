f4171b12c3d048d225ef255b29c29b4e902aedb6
adityaastic <adityaastic@gmail.com>
2024-04-21 20:48:21 +0530

    Build sign in page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
+import Body from './components/Body';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="font-bold">
-      <h2>Hello World</h2>
-      </div>
+    <div className=''>
+      <Header />
+      <Body />
+    </div>
   );
 }
 export default App;
