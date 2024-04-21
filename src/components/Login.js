714aaac2f76401bf2ab41488161b57b6d6bb1a54
adityaastic <adityaastic@gmail.com>
2024-04-22 01:37:53 +0530

    Navigate bug fixed

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,21 @@
 import React, { useRef, useState } from "react";
 import { checkValidateData } from "../utils/validation";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    updateProfile
+} from "firebase/auth";
 import { auth } from "../utils/firebase";
-import { useNavigate } from "react-router-dom";
 import Header from "./Header";
+import { useDispatch } from "react-redux";
+import { adduser } from "../utils/userSlice";
 
 const Login = () => {
     const [isSigninForm, setIsSignInForm] = useState(true);
     const [errorMessage, setErrorMessage] = useState(null);
-    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
-    // const name = useRef(null);
+    const name = useRef(null);
     const email = useRef(null);
     const password = useRef(null);
     // Form validation
@@ -27,13 +32,22 @@ const Login = () => {
                 .then((userCredential) => {
                     // Signed up 
                     const user = userCredential.user;
+                    updateProfile(user, {
+                        displayName: name.current.value
+                    }).then(() => {
+                        // Profile updated!
+                        const { uid, email, displayName } = auth.currentUser;
+                        dispatch(adduser({ uid: uid, email: email, displayName: displayName }))
+                    }).catch((error) => {
+                        setErrorMessage(error.message);
+                    });
                     console.log(user);
-                    navigate("/browse")
                 })
                 .catch((error) => {
-                    const errorCode = error.code;
-                    const errorMessage = error.message;
-                    setErrorMessage(errorCode + "-" + errorMessage);
+                    // const errorCode = error.code;
+                    // const errorMessage = error.message;
+                    // setErrorMessage(errorCode + "-" + errorMessage);
+                    setErrorMessage("Something went wrong.")
                 });
         } else {
             // Sign in logic
@@ -42,16 +56,15 @@ const Login = () => {
                     // Signed in 
                     const user = userCredential.user;
                     console.log(user);
-                    navigate("/browse")
                 })
                 .catch((error) => {
-                    const errorCode = error.code;
-                    const errorMessage = error.message;
-                    setErrorMessage(errorCode + " - " + errorMessage);
+                    // const errorCode = error.code;
+                    // const errorMessage = error.message;
+                    // setErrorMessage(errorCode + " - " + errorMessage);
+                    setErrorMessage("Please Sign up.")
                 });
         }
     };
-
     const toggleSigninForm = () => {
         setIsSignInForm(!isSigninForm)
     }
@@ -66,7 +79,7 @@ const Login = () => {
                 <h1 className="text-xl font-semibold">{isSigninForm ? "Sign In" : "Sign Up"}</h1>
                 {!isSigninForm &&
                     <input
-                        // ref={name}
+                        ref={name}
                         type="name"
                         placeholder="Name"
                         className=" p-3 my-2 w-full rounded-md bg-gray-700"
@@ -96,7 +109,7 @@ const Login = () => {
                         Sign up now.
                     </p> :
                         <div className="flex">
-                            <p className="text-gray-400 text-sm"> Already registered? </p>
+                            <p className="text-gray-400 text-sm"> Already have an account? </p>
                             <p className="text-gray-200 text-sm cursor-pointer px-2 hover:underline"
                                 onClick={toggleSigninForm}>
                                 Sign In now.
