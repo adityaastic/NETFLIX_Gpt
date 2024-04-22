3764916df2ad78a49c79996c5d57fe2b3e408bdf
adityaastic <adityaastic@gmail.com>
2024-04-22 09:50:26 +0530

    Background video and movieList changes

--- src/components/secondaryContainer/SecondaryContainer.js
+++ src/components/secondaryContainer/SecondaryContainer.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import MovieList from "./MovieList";
-
 const SecondaryContainer = () => {
     const movies = useSelector((store) => store.movies)
 
     return (
-        <div>
-            <MovieList title={"Now playing Movie"} movies={movies?.nowPlayingMovies} />
+        <div className="w-screen">
+            <div className="">
+                <MovieList title={"Now playing Movies"} movies={movies?.nowPlayingMovies} />
+                <MovieList title={"Trending movies"} movies={movies?.nowPlayingMovies} />
+                <MovieList title={"Horror Movies"} movies={movies?.nowPlayingMovies} />
+                <MovieList title={"Upcoming movies"} movies={movies?.nowPlayingMovies} />
+            </div>
+
         </div>
     );
 };
